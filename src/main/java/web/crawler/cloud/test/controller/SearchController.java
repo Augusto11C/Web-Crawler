c2bc231284f82440eaec1613dd06b5c4cd49cedc
AugustoCalado <augusto.bueno@usp.br>
2019-04-06 22:11:20 -0300

    Added new time feature in web crawler

--- src/main/java/web/crawler/cloud/test/controller/SearchController.java
+++ src/main/java/web/crawler/cloud/test/controller/SearchController.java
@@ -24,21 +24,19 @@ public class SearchController {
 
     @RequestMapping(value = "crawler_param", method = RequestMethod.POST)
     @ResponseBody
-    public List<String> crawlerTest(@RequestParam String urlToCrawler2) {
-        System.out.println(urlToCrawler2);
-/*
-        if (urlToCrawler != null) {
-            webCrawler.getPageUrls(urlToCrawler);
+    public List<String> crawlerTest(@RequestParam String urlToCrawler, @RequestParam int crawlerDuration) {
+
+        if (urlToCrawler != null ) {
+
+            System.out.println(crawlerDuration);
+
+            webCrawler.getPageUrls(urlToCrawler,crawlerDuration);
             while (webCrawler.getUrls().size() < 10) ;
 
             return convertToListOfStrings(webCrawler.getUrls());
         }
 
-        return null;*/
-        webCrawler.getPageUrls("https://www.mkyong.com/spring-boot/spring-boot-hello-world-example-thymeleaf/");
-
-//        return Arrays.asList("Buenos Aires", "Córdoba", "La Plata");
-        return convertToListOfStrings(webCrawler.getUrls());
+        return null;
 
     }
 
--- src/main/java/web/crawler/cloud/test/crawler/WebCrawler.java
+++ src/main/java/web/crawler/cloud/test/crawler/WebCrawler.java
@@ -17,7 +17,7 @@ import java.util.HashSet;
 @Service
 public class WebCrawler {
 
-    static final int TIMEOUT = 30000;   // one minute
+    private int timeout = 60000;   // one minute
     private HashSet<String> urls;
 
     public WebCrawler() {
@@ -27,7 +27,7 @@ public class WebCrawler {
     @Autowired
     UrlRepository urlRepository;
 
-    public void getPageUrls(String url) {
+    public void getPageUrls(String url, int duration) {
 
         if (urls.size() > 10) {
             return;
@@ -35,26 +35,20 @@ public class WebCrawler {
 
         if (!urls.contains(url) && url != null) {
             try {
+
+                timeout = duration;
                 urls.add(url);
+
+                //Saving on BD
                 Url newUrl = new Url(url);
                 urlRepository.save(newUrl);
 
-
-
-/*                Document mydocument = Jsoup.connect(url).get();
-                Element link = mydocument.select("a").first();
-
-                System.out.println("Relative Link: " + link.attr("href"));
-                System.out.println("Absolute Link: " + link.attr("abs:href"));
-                System.out.println("Absolute Link: " + link.absUrl("href"));
-*/
-                //Document document = Jsoup.connect(url).get();
-                Document document = Jsoup.parse(new URL(url),TIMEOUT);
+                Document document = Jsoup.parse(new URL(url),timeout);
                 Elements urlsFromPage = document.select("a[href]");
 
                 for (Element oneUrlFromPage : urlsFromPage) {
 
-                    getPageUrls(oneUrlFromPage.attr("abs:href"));
+                    getPageUrls(oneUrlFromPage.attr("abs:href"), timeout);
                 }
 
             } catch (Exception e) {
@@ -70,7 +64,4 @@ public class WebCrawler {
         return urls;
     }
 
-    public void setUrls(HashSet<String> urls) {
-        this.urls = urls;
-    }
 }
